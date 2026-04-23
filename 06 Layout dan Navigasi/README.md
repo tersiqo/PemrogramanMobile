205c7006c9b8761297b81940807915aa7b10f595
tersiqo <tersiqo.alfarezel@gmail.com>
2026-04-23 16:10:15 +0700

    Praktikum 6

--- 06 Layout dan Navigasi/README.md
+++ 06 Layout dan Navigasi/README.md
@@ -15,46 +15,50 @@ Absen   : 21 <br>
 3. Langkah 3: Identifikasi layout diagram <br>
 
 4. Langkah 4: Implementasi title row<br>
-![prak1langkah4](img/tugas_prak1/Praktikum1Langkah4.png)<br>
-![prak1langkah4](img/tugas_prak1/Praktikum1Langkah4-2.png)<br>
-![prak1langkah4](img/tugas_prak1/Praktikum1Langkah4-3.jpeg)<br>
+![prak1langkah4](img/Praktikum_Langkah_4.png)<br>
+![prak1langkah4](img/Praktikum_Langkah_5.png)<br>
+Hasil<br>
+![prak1langkah4](img/Praktikum_Langkah_6.jpg)<br>
 
 
 ## Praktikum 2: Implementasi button row
 1. Langkah 1: Buat method Column _buildButtonColumn<br>
 - lib/main.dart (_buildButtonColumn) <br>
-![prak2langkah1](img/tugas_prak1/Praktikum2Langkah1.png)<br>
+![prak2langkah1](img/Praktikum_Langkah_7.png)<br>
 
 2. Langkah 2: Buat widget buttonSection<br>
 - lib/main.dart (buttonSection)<br>
-![prak2langkah2](img/tugas_prak1/Praktikum2Langkah2.png)<br>
+![prak2langkah2](img/Praktikum_Langkah_8.png)<br>
 
 3. Langkah 3: Tambah button section ke body<br>
 - Tambahkan variabel buttonSection ke dalam body seperti berikut:<br>
-![prak2langkah3](img/tugas_prak1/Praktikum2Langkah3.png)<br>
-![prak2langkah3](img/tugas_prak1/Praktikum2Langkah3-2.jpeg)<br>
+![prak2langkah3](img/Praktikum_Langkah_9.png)<br>
+Hasil<br>
+![prak2langkah3](img/Praktikum_Langkah_10.jpg)<br>
 
 
 ## Praktikum 3: Implementasi text section
 1. Buat widget textSection<br>
-![prak3langkah1](img/tugas_prak1/Praktikum3Langkah1.png)<br>
+![prak3langkah1](img/Praktikum_Langkah_11.png)<br>
 
 2. Tambahkan variabel text section ke body<br>
-![prak3langkah2](img/tugas_prak1/Praktikum3Langkah2.png)<br>
-![prak3langkah2](img/tugas_prak1/Praktikum3Langkah2-2.jpeg)<br>
+![prak3langkah2](img/Praktikum_Langkah_12.png)<br>
+Hasil<br>
+![prak3langkah2](img/Praktikum_Langkah_13.jpg)<br>
 
 ## Praktikum 4: Implementasi image section
 1. Langkah 1: Siapkan aset gambar<br>
 - Isi file pubspec.yaml<br>
-![prak4langkah1](img/tugas_prak1/Praktikum4Langkah1.png)<br>
+![prak4langkah1](img/Praktikum_Langkah_14.png)<br>
 
 2. Langkah 2: Tambahkan gambar ke body <br>
 - Tambahkan aset gambar ke dalam body seperti berikut:<br>
-![prak4langkah2](img/tugas_prak1/Praktikum4Langkah2.png)<br>
+![prak4langkah2](img/Praktikum_Langkah_15.png)<br>
 
 3. Langkah 3: Terakhir, ubah menjadi ListView <br>
-![prak4langkah3](img/tugas_prak1/Praktikum4Langkah3.png)<br>
-![prak4langkah3](img/tugas_prak1/Praktikum4Langkah3-2.jpeg)<br>
+![prak4langkah3](img/Praktikum_Langkah_16.png)<br>
+Hasil<br>
+![prak4langkah3](img/Praktikum_Langkah_17.jpgg)<br>
 
 ## Tugas Praktikum 1
 1. Selesaikan Praktikum 1 sampai 4, lalu dokumentasikan dan push ke repository Anda berupa screenshot setiap hasil pekerjaan beserta penjelasannya di file README.md!
--- 06 Layout dan Navigasi/layout_flutter/lib/main.dart
+++ 06 Layout dan Navigasi/layout_flutter/lib/main.dart
@@ -7,16 +7,108 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+     Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Gunung di kawasan Batu, Malang merupakan salah satu destinasi wisata '
+        'alam yang populer di Jawa Timur. Daerah ini menawarkan pemandangan '
+        'pegunungan yang indah, udara yang sejuk, serta berbagai aktivitas '
+        'outdoor seperti hiking dan camping. Banyak wisatawan datang untuk '
+        'menikmati keindahan alam serta spot foto yang menarik.\n\n'
+        'Nama: Tersiqo Alfarezel\n'
+        'NIM: 244107060089',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Tersiqo Alfarezel 244107060089',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/cangar.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
       ),
+    );
+  }
+  
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
